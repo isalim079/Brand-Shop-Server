9f8d1629ba89b7a0934953c387f958fbc672afc3
isalim079 <salimreza1025@gmail.com>
2023-10-21 06:54:56 +0600

    404 error for 3 reload updated

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const brandShopCollection = client
             .db("brandShopDB")
@@ -69,7 +69,7 @@ async function run() {
         // For Delete
         app.delete("/brandShopCarts/:id", async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await brandShopCartCollection.deleteOne(query);
             res.send(result);
         });
@@ -118,10 +118,10 @@ async function run() {
         });
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log(
-            "Pinged your deployment. You successfully connected to MongoDB!"
-        );
+        // await client.db("admin").command({ ping: 1 });
+        // console.log(
+        //     "Pinged your deployment. You successfully connected to MongoDB!"
+        // );
     } finally {
         // Ensures that the client will close when you finish/error
     }
